b3e608713abf4322597b0949f23993dab858241b
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2023-02-06 14:13:29 +0900

    헤더, 메인 배너, 서브 페이지 about 추가

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// main.js
+
+$(function () {
+  $(".main-banner2 h2").find("strong").css("font-size", "64px");
+});
+
+$(function () {
+  $(".main-banner2 h2").find("span").css("color", "var(--point)");
+});
