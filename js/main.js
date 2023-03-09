b3d643eada6a74712555818db1843c8f4cac81ca
myeongakim7 <113976428+myeongakim7@users.noreply.github.com>
2023-03-09 17:10:17 +0900

    -포트폴리오 이미지 호버 시 확대 효과
    -포트폴리오 이미지 호버 시 확대 효과 
    -스크린샷 추가 
    -구글 폰트 추가

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 // main.js
 
-$(function () {
-  $(".main-banner2 h2").find("strong").css("font-size", "64px");
-});
+// pc화면
+if (screen.width > 768) {
+  $(function () {
+    $(".main-banner2 h2").find("strong").css("font-size", "64px");
+  });
 
-$(function () {
-  $(".main-banner2 h2").find("span").css("color", "var(--point)");
-});
+  $(function () {
+    $(".main-banner2 h2").find("span").css("color", "var(--point)");
+  });
+}
+
+// mobile 화면
+if (screen.width < 769) {
+  $(function () {
+    $(".main-banner2 h2").find("strong").css("font-size", "24px");
+  });
+  $(function () {
+    $(".main-banner2 h2").find("span").css("color", "var(--point)");
+  });
+}
